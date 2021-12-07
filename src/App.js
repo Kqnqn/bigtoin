115486929bac81c0363f8d70a5a6349aaf30b7e6
keno <ken.hadz@gmail.com>
2021-12-07 17:37:05 +0100

    Implemented page switch

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Coinpage from "./pages/Coinpage";
+import Homepage from "./pages/Homepage";
 
 function App() {
-  return <div className="App">Halso</div>;
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Homepage />} exact></Route>
+        </Routes>
+        <Routes>
+          <Route path="/coins" element={<Coinpage />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
+
+{
+  /* <Routes>
+          <Route path="/" element={<Homepage />}></Route>
+        </Routes>
+        <Routes>
+          <Route path="/" element={<Coinpage />}></Route>
+        </Routes> */
+}
---
+++ src/pages/Coinpage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Coinpage() {
+  return <div>Coin</div>;
+}
+
+export default Coinpage;
