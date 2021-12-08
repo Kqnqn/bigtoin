502ab4fe188e4e9ed9a2b3d8d36556f4a4211293
keno <ken.hadz@gmail.com>
2021-12-08 16:09:20 +0100

    grozno

--- src/App.js
+++ src/App.js
@@ -3,11 +3,22 @@ import "./App.css";
 import Header from "./components/Header";
 import Coinpage from "./pages/Coinpage";
 import Homepage from "./pages/Homepage";
+import { makeStyles } from "@material-ui/core/styles";
 
 function App() {
+  const useStyles = makeStyles(() => ({
+    App: {
+      backgroundColor: "#14161a",
+      minHeight: "100vh",
+      color: "#FFFFFF",
+    },
+  }));
+
+  const classes = useStyles();
+
   return (
     <BrowserRouter>
-      <div>
+      <div className={classes.App}>
         <Header />
         <Routes>
           <Route path="/" element={<Homepage />} exact></Route>
@@ -21,12 +32,3 @@ function App() {
 }
 
 export default App;
-
-{
-  /* <Routes>
-          <Route path="/" element={<Homepage />}></Route>
-        </Routes>
-        <Routes>
-          <Route path="/" element={<Coinpage />}></Route>
-        </Routes> */
-}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,53 @@
+import {
+  AppBar,
+  Container,
+  MenuItem,
+  Select,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { useNavigate } from "react-router-dom";
+
+const useStyles = makeStyles(() => ({
+  title: {
+    fontFamily: "Montserrat",
+    fontWeight: "bold",
+    cursor: "pointer",
+    color: "gold",
+  },
+}));
 
 function Header() {
-  return <div>Header</div>;
+  const classes = useStyles();
+  const navigate = useNavigate();
+
+  return (
+    <AppBar color="transparent" position="static">
+      <Container>
+        <Toolbar>
+          <Typography
+            onClick={() => navigate("/home")}
+            className={classes.title}
+          >
+            Bigtoin
+          </Typography>
+          <Select
+            variant="outlined"
+            style={{
+              width: 100,
+              heigh: 40,
+              marginLeft: 15,
+            }}
+          >
+            <MenuItem value={"USD"}>USD</MenuItem>
+            <MenuItem value={"BAM"}>BAM</MenuItem>
+          </Select>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
 }
 
 export default Header;
